ee9d932fed53c7af1d0f464fbc4779fc627877cc
Blake Kostner <btkostner@gmail.com>
2019-06-09 21:18:58 -0600

    test: fix houston dashboard page find test

--- apps/houston_dashboard/test/houston_web/controllers/page_controller_test.exs
+++ apps/houston_dashboard/test/houston_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule HoustonDashboard.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Publish on AppCenter"
   end
 end
